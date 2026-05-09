bc9a29abaa5e9470cfd45696b89914201b3a7805
Muhammad Ramadhiansyah <ramaaka676@gmail.com>
2026-05-09 09:20:42 +0700

    debug app value

--- api/index.php
+++ api/index.php
@@ -1,29 +1,8 @@
 <?php
 
-// Fix untuk Vercel read-only filesystem
-$dirs = [
-    '/tmp/storage/logs',
-    '/tmp/storage/framework/cache/data',
-    '/tmp/storage/framework/sessions',
-    '/tmp/storage/framework/views',
-    '/tmp/storage/app/public',
-];
+$bootstrap = __DIR__ . '/../bootstrap/app.php';
+$app = require $bootstrap;
 
-foreach ($dirs as $dir) {
-    if (!is_dir($dir)) {
-        mkdir($dir, 0775, true);
-    }
-}
-
-$_ENV['APP_STORAGE'] = '/tmp/storage';
-putenv('APP_STORAGE=/tmp/storage');
-
-$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-
-if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
-    return false;
-}
-
-chdir(__DIR__ . '/../public');
-
-require __DIR__ . '/../public/index.php';
+var_dump(gettype($app));
+var_dump($app);
+exit;
