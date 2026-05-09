a7685a7c98bb441212516d0d79d1b386662c9e04
Muhammad Ramadhiansyah <ramaaka676@gmail.com>
2026-05-09 10:00:14 +0700

    test autoload

--- api/index.php
+++ api/index.php
@@ -1,2 +1,25 @@
 <?php
-echo json_encode(['status' => 'PHP is working', 'version' => phpversion()]);
+
+$dirs = [
+    '/tmp/storage/logs',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/testing',
+    '/tmp/storage/app/public',
+    '/tmp/bootstrap/cache',
+];
+
+foreach ($dirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0775, true);
+    }
+}
+
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+putenv('APP_STORAGE=/tmp/storage');
+
+require __DIR__ . '/../vendor/autoload.php';
+
+echo json_encode(['status' => 'autoload ok']);
