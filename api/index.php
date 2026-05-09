29975f924edbea40408c4eda395781bf9a8afe1d
Muhammad Ramadhiansyah <ramaaka676@gmail.com>
2026-05-09 09:10:21 +0700

    fix storage path laravel 12 vercel

--- api/index.php
+++ api/index.php
@@ -1,14 +1,16 @@
 <?php
 
-// Bridge ke Laravel public/index.php
+// Set storage path ke /tmp karena Vercel read-only
+$_ENV['APP_STORAGE'] = '/tmp';
+
+ini_set('display_errors', '1');
+
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
-// Serve static files jika ada
 if ($uri !== '/' && file_exists(__DIR__ . '/../public' . $uri)) {
     return false;
 }
 
-// Arahkan semua request ke Laravel
 $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
 $_SERVER['SCRIPT_NAME'] = '/index.php';
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,10 +1,8 @@
-<?php
-
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -17,3 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+// Fix storage path untuk Vercel (filesystem read-only)
+$app->useStoragePath(env('APP_STORAGE', $app->storagePath()));
+
+return $app;
