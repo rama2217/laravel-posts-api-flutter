d223ae4331cd3588d9a1ca352d19947a6717c75d
Muhammad Ramadhiansyah <ramaaka676@gmail.com>
2026-05-09 10:20:33 +0700

    symlink bootstrap cache to tmp

--- api/index.php
+++ api/index.php
@@ -23,20 +23,17 @@ putenv('APP_STORAGE=/tmp/storage');
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Patch PackageManifest sebelum app dibuat
+// dengan symlink bootstrap/cache ke /tmp/bootstrap/cache
+$originalCache = __DIR__ . '/../bootstrap/cache';
+if (!is_link($originalCache) && !is_writable($originalCache)) {
+    // Buat symlink dari bootstrap/cache ke /tmp/bootstrap/cache
+    @symlink('/tmp/bootstrap/cache', $originalCache);
+}
+
 try {
     $app = require __DIR__ . '/../bootstrap/app.php';
     $app->useStoragePath('/tmp/storage');
-    $app->instance('path.bootstrap', '/tmp/bootstrap');
-
-    // Bootstrap aplikasi dulu agar service providers ter-register
-    $app->bootstrapWith([
-        \Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class,
-        \Illuminate\Foundation\Bootstrap\LoadConfiguration::class,
-        \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
-        \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
-        \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
-        \Illuminate\Foundation\Bootstrap\BootProviders::class,
-    ]);
 
     $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
     $request = Illuminate\Http\Request::capture();
