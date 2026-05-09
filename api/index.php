265de4b9eb8d0e2e43d582d3fc0b0f6d52249375
Muhammad Ramadhiansyah <ramaaka676@gmail.com>
2026-05-09 09:24:16 +0700

    debug with autoloader

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 
+// Load composer autoloader dulu
+require __DIR__ . '/../vendor/autoload.php';
+
 $bootstrap = __DIR__ . '/../bootstrap/app.php';
 $app = require $bootstrap;
 
 var_dump(gettype($app));
-var_dump($app);
+var_dump(get_class($app));
 exit;
